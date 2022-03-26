836bc61f93718a70a4b6ce6c28e656ca8241c6c8
Sean McQueen <sean@macbook-pro.myfiosgateway.com>
2022-03-26 14:59:02 -0400

    Add barebones details back to React App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 const App = () => ( 
-    <div>
-        <h1>Hello React</h1>
-    </div>
+  <div>
+    <h1>hi, I'm Sean McQueen</h1>
+    <p>I live in New York City and manage engineers building the machine learning platform at Twitter</p>
+    <p>
+        <a href="files/sean_mcqueen.pdf">Check out my resume</a>,
+        or find me on <a href="https://twitter.com/seanmcque_n">Twitter</a> (or <a href="https://www.linkedin.com/in/snmcqueen">LinkedIn</a>)
+    </p>
+    <p>If you want to contact me directly, send me an email: sean@mcqueen.net </p>
+  </div>
 );
 
 export default App;
