f3d0e2d9201bfbb2e3c0899ffae8f349b19c3ca1
Sean McQueen <sean@launchscience.com>
2024-01-26 10:12:38 -0800

    Update 2024

--- src/App.js
+++ src/App.js
@@ -26,10 +26,8 @@ const App = () => {
       >
         <div className="ContentBox" key='links' data-grid={{ x: 0, y: 0, w: 2, h: 2, static: true }}>
           <Holder>
-            <p><a href="files/sean_mcqueen.pdf">Check out my resume</a>.</p>
-            <p>Find me on <a href="https://twitter.com/seanmcque_n">Twitter</a> or <a href="https://www.linkedin.com/in/snmcqueen">LinkedIn</a>.</p>
-            <p>Or email me: </p>
-            <p><em>sean@mcqueen.net</em></p>
+            <p>Check out <a href="files/sean_mcqueen.pdf">my resume</a> or find me on <a href="https://www.linkedin.com/in/snmcqueen">LinkedIn</a>.</p>
+            <p>Send me an email: <em>sean@mcqueen.net</em></p>
           </Holder>
         </div>
         <div className="ContentBox" key='header' style={{ textAlign: 'right' }} data-grid={{ x: 2, y: 0, w: 1, h: 2 }}>
@@ -39,7 +37,7 @@ const App = () => {
         </div>
         <div className="ContentBox" key='about_me' data-grid={{ x: 3, y: 1, w: 4, h: 1 }}>
           <Holder>
-            <p>I live in New York City and I'm building something new.</p>
+            <p>I live in Seattle, I lead software teams and I'm looking for a new role.</p>
           </Holder>
         </div>
         <div className="ContentBox" key='how_to' data-grid={{ x: 3, y: 0, w: 2, h: 1 }}>
